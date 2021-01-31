6fa3fb301e15073fd6939920b45e85550faf6485
Joyce Quach <jtquach1@gmail.com>
2021-01-31 07:52:14 -0500

    Create Alphabet.java, updated cs498.iml

---
+++ src/main/algorithms/Alphabet.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+public class Alphabet {
+    private HashSet symbols;
+
+    public HashSet getSymbols() {
+        return symbols;
+    }
+
+    public void setSymbols(HashSet symbols) {
+        this.symbols = symbols;
+    }
+}
