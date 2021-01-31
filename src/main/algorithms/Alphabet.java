096898d0dc0417f95eeeecb68bc8ee60b1191a41
Joyce Quach <jtquach1@gmail.com>
2021-01-31 09:00:52 -0500

    Add main function to Alphabet, update .gitignore

--- src/main/algorithms/Alphabet.java
+++ src/main/algorithms/Alphabet.java
@@ -1,14 +1,21 @@
-import java.util.Arrays;
 import java.util.HashSet;
 
 public class Alphabet {
-    private HashSet<String> symbols;
+    private HashSet<Character> symbols = new HashSet<>();
 
-    public HashSet<String> getSymbols() {
+    public static void main(String[] args) {
+        Alphabet alphabet = new Alphabet();
+        alphabet.addSymbol('a');
+        System.out.println(alphabet);
+        alphabet.addSymbol('b');
+        System.out.println(alphabet);
+    }
+
+    public HashSet<Character> getSymbols() {
         return symbols;
     }
 
-    public void addSymbol(String newSymbol) {
+    public void addSymbol(Character newSymbol) {
         symbols.add(newSymbol);
     }
 
