962d8be2aeca2d660a7019fd98947e23666e77eb
jtquach1 <jtquach1@gmail.com>
2021-01-31 08:14:27 -0500

    Update Alphabet.java

--- src/main/algorithms/Alphabet.java
+++ src/main/algorithms/Alphabet.java
@@ -1,13 +1,19 @@
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class Alphabet {
-    private HashSet symbols;
+    private HashSet<String> symbols;
 
-    public HashSet getSymbols() {
+    public HashSet<String> getSymbols() {
         return symbols;
     }
 
-    public void setSymbols(HashSet symbols) {
-        this.symbols = symbols;
+    public void addSymbol(String newSymbol) {
+        symbols.add(newSymbol);
+    }
+
+    @Override
+    public String toString() {
+        return symbols.toString();
     }
 }
