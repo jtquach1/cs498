add86da79ef99e01a047f687b0fc263d4ae695d1
Joyce Quach <jtquach1@gmail.com>
2021-02-02 18:09:05 -0500

    Draft regex methods for NFA.java

--- src/main/algorithms/NFA.java
+++ src/main/algorithms/NFA.java
@@ -1,4 +1,5 @@
 public class NFA extends FSA {
+    private NFA nfa;
     public static final char EPSILON = '\u025B';
     public static final Character[] reservedCharacters = new Character[]{'|', '(', ')', '*', EPSILON};
 
@@ -6,4 +7,20 @@ public class NFA extends FSA {
         NFA nfa = new NFA();
         return nfa;
     }
+
+    public static NFA concatenate(NFA self, NFA other, Character literal) {
+        NFA nfa = new NFA();
+        return nfa;
+    }
+
+    public static NFA kleeneStar(NFA self) {
+        NFA nfa = new NFA();
+        return nfa;
+    }
+
+    public static NFA alternate(NFA self, NFA other) {
+        NFA nfa = new NFA();
+        return nfa;
+    }
+
 }
