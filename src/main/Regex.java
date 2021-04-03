0c7d5635db18cb02c01a9dbd29e597c08b47d006
Joyce Quach <jtquach1@gmail.com>
2021-04-03 13:17:19 -0400

    Update unit tests and .gitignore - Add Regex unit tests

--- src/main/Regex.java
+++ src/main/Regex.java
@@ -1,6 +1,11 @@
 import org.jetbrains.annotations.NotNull;
 
 class Regex {
+    static String infixToPostfix(String infix) {
+        infix = markWithConcatenation(infix);
+        return infixToPostfix(infix.toCharArray());
+    }
+
     static String markWithConcatenation(String originalInfix) {
         char[] infix = originalInfix.toCharArray();
         Queue<Character> temp = new Queue<>();
@@ -14,6 +19,16 @@ class Regex {
         }
 
         return generateInfixString(temp);
+    }
+
+    public static void main(String[] args) {
+        String originalInfix = "(cd*|b)*a";
+        System.out.println("infix: " + Regex.markWithConcatenation(originalInfix));
+        System.out.println("postfix: " + Regex.infixToPostfix(originalInfix));
+
+        originalInfix = "a(bb)a";
+        System.out.println("infix: " + Regex.markWithConcatenation(originalInfix));
+        System.out.println("postfix: " + Regex.infixToPostfix(originalInfix));
     }
 
     private static boolean canConcatenate(char[] infixArray, int limit, int index) {
@@ -33,83 +48,8 @@ class Regex {
         return false;
     }
 
-    public static void main(String[] args) {
-        String originalInfix = "(cd*|b)*a";
-        System.out.println("infix: " + Regex.markWithConcatenation(originalInfix));
-        System.out.println("postfix: " + Regex.infixToPostfix(originalInfix));
-
-        originalInfix = "a(bb)a";
-        System.out.println("infix: " + Regex.markWithConcatenation(originalInfix));
-        System.out.println("postfix: " + Regex.infixToPostfix(originalInfix));
-    }
-
-    private static boolean isOperand(char c) {
-        return Character.isAlphabetic(c) || Character.isDigit(c);
-    }
-
-    private static boolean isOperator(char c) {
-        return c == '.' | c == '*' | c == '|';
-    }
-
-    private static boolean hasGreaterPrecedence(Character top, Character token) {
-        return getPrecedence(top) > getPrecedence(token);
-    }
-
-    private static boolean hasEqualPrecedence(Character top, Character token) {
-        return getPrecedence(top) == getPrecedence(token);
-    }
-
-    private static int getPrecedence(Character operator) {
-        // Default is unimplemented operator
-        int precedence;
-
-        switch (operator) {
-            case '.':
-                precedence = 1;
-                break;
-            case '|':
-                precedence = 2;
-                break;
-            case '*':
-                precedence = 3;
-                break;
-            default:
-                precedence = 0;
-                break;
-        }
-
-        return precedence;
-    }
-
-    private static boolean isLeftParenthesis(Character top) {
-        return top == '(';
-    }
-
-    private static boolean isRightParenthesis(Character top) {
-        return top == ')';
-    }
-
     private static boolean isStar(Character top) {
         return top == '*';
-    }
-
-    private static boolean isLeftAssociative(Character token) {
-        // | is reflexive
-        return token == '.' || token == '*';
-    }
-
-
-    @NotNull
-    private static String generatePostfixString(Stack<Character> input) {
-        StringBuilder sb = new StringBuilder();
-        Stack<Character> reverse = new Stack<>();
-        while (!input.isEmpty()) {
-            reverse.push(input.pop());
-        }
-        while (!reverse.isEmpty()) {
-            sb.append(reverse.pop());
-        }
-        return sb.toString();
     }
 
     @NotNull
@@ -121,53 +61,7 @@ class Regex {
         return sb.toString();
     }
 
-    private static void handleLeftParenthesis(Stack<Character> operators, char token) {
-        operators.push(token);
-    }
-
-    private static void handleRightParenthesis(Stack<Character> postfix, Stack<Character> operators) {
-        Character top = operators.peek();
-        while (!isLeftParenthesis(top)) {
-            postfix.push(operators.pop());
-            top = operators.peek();
-        }
-        top = operators.peek();
-        if (isLeftParenthesis(top)) {
-            operators.pop();
-        }
-    }
-
-    private static void handleOperand(Stack<Character> postfix, char token) {
-        postfix.push(token);
-    }
-
-    private static void handleOperator(Stack<Character> postfix, Stack<Character> operators, char token) {
-        Character top = operators.peek();
-        boolean existsTopOperator = top != null;
-        while (existsTopOperator
-                && (hasGreaterPrecedence(top, token)
-                || (hasEqualPrecedence(top, token) && isLeftAssociative(token)))
-                && !isLeftParenthesis(top)
-        ) {
-            postfix.push(operators.pop());
-            top = operators.peek();
-            existsTopOperator = top != null;
-        }
-        operators.push(token);
-    }
-
-    private static void handleRemainingOperators(Stack<Character> postfix, Stack<Character> operators) {
-        while (!operators.isEmpty()) {
-            postfix.push(operators.pop());
-        }
-    }
-
-    static String infixToPostfix(String infix) {
-        infix = markWithConcatenation(infix);
-        return infixToPostfix(infix.toCharArray());
-    }
-
-    static String infixToPostfix(char[] infix) {
+    private static String infixToPostfix(char[] infix) {
         // Following Dijkstra's Shunting-Yard Algorithm
         Stack<Character> postfix = new Stack<>();
         Stack<Character> operators = new Stack<>();
@@ -186,5 +80,112 @@ class Regex {
 
         handleRemainingOperators(postfix, operators);
         return generatePostfixString(postfix);
+    }
+
+    private static boolean isOperand(char c) {
+        return Character.isAlphabetic(c) || Character.isDigit(c);
+    }
+
+    private static void handleOperand(Stack<Character> postfix, char token) {
+        postfix.push(token);
+    }
+
+    private static boolean isOperator(char c) {
+        return c == '.' | c == '*' | c == '|';
+    }
+
+    private static void handleOperator(Stack<Character> postfix, Stack<Character> operators, char token) {
+        Character top = operators.peek();
+        boolean existsTopOperator = top != null;
+        while (existsTopOperator
+                && (hasGreaterPrecedence(top, token)
+                || (hasEqualPrecedence(top, token) && isLeftAssociative(token)))
+                && !isLeftParenthesis(top)
+        ) {
+            postfix.push(operators.pop());
+            top = operators.peek();
+            existsTopOperator = top != null;
+        }
+        operators.push(token);
+    }
+
+
+    private static boolean hasGreaterPrecedence(Character top, Character token) {
+        return getPrecedence(top) > getPrecedence(token);
+    }
+
+    private static boolean hasEqualPrecedence(Character top, Character token) {
+        return getPrecedence(top) == getPrecedence(token);
+    }
+
+    private static boolean isLeftAssociative(Character token) {
+        // | is reflexive
+        return token == '.' || token == '*';
+    }
+
+    private static int getPrecedence(Character operator) {
+        int precedence;
+
+        switch (operator) {
+            case '.':
+                precedence = 1;
+                break;
+            case '|':
+                precedence = 2;
+                break;
+            case '*':
+                precedence = 3;
+                break;
+
+            // Unimplemented operator
+            default:
+                precedence = 0;
+                break;
+        }
+
+        return precedence;
+    }
+
+    private static boolean isLeftParenthesis(Character top) {
+        return top == '(';
+    }
+
+    private static void handleLeftParenthesis(Stack<Character> operators, char token) {
+        operators.push(token);
+    }
+
+    private static boolean isRightParenthesis(Character top) {
+        return top == ')';
+    }
+
+    private static void handleRightParenthesis(Stack<Character> postfix, Stack<Character> operators) {
+        Character top = operators.peek();
+        while (!isLeftParenthesis(top)) {
+            postfix.push(operators.pop());
+            top = operators.peek();
+        }
+        top = operators.peek();
+        if (isLeftParenthesis(top)) {
+            operators.pop();
+        }
+    }
+
+    private static void handleRemainingOperators(Stack<Character> postfix, Stack<Character> operators) {
+        while (!operators.isEmpty()) {
+            postfix.push(operators.pop());
+        }
+    }
+
+    @NotNull
+    private static String generatePostfixString(Stack<Character> input) {
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> reverse = new Stack<>();
+        while (!input.isEmpty()) {
+            reverse.push(input.pop());
+        }
+        while (!reverse.isEmpty()) {
+            sb.append(reverse.pop());
+        }
+        return sb.toString();
     }
 }
