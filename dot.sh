eb84756e78ac67277f3b4d66a60f61317a3be2b8
Joyce Quach <jtquach1@gmail.com>
2021-08-25 00:16:56 -0400

    Use Maven over Ant for building project

--- dot.sh
+++ dot.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-input_dir=/home/jtquach/cs498
-retrieval_dir=/home/jtquach/jtqua/Documents
-
 function split {
    name="${1%.*}"
-   ext="${1##*.}"
-   # echo filename=$name extension=$ext
    echo $name
 }
 
-mv $input_dir/*.dot .
 echo "Converting DOT to PDF"
 for FILE in `ls *.dot`
 do
@@ -19,6 +13,3 @@ do
 	dot -Tpdf $FILE -o $prefix.pdf
 	echo $prefix.pdf
 done
-
-mv *.dot $retrieval_dir
-mv *.pdf $retrieval_dir
